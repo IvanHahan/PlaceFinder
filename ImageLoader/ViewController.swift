54dee3e8265b7cdfe9dcf62b49366c6d3127f408
Ivan Hahanov <ivanhahanov@icloud.com>
2018-03-10 22:03:06 +0200

    Initial Commit

---
+++ ImageLoader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageLoader
+//
+//  Created by Ivan Hahanov on 3/10/18.
+//  Copyright © 2018 Ivan Hahanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
